e989704c97b3afaaf01beff00f9c9c2ba82fd5fd
Navaneeth Karumuru <naveen50003@gmail.com>
2024-02-03 11:06:43 +0530

    removed unwanted line

--- ingestion.py
+++ ingestion.py
@@ -1,7 +1,6 @@
 import os
 from langchain_community.document_loaders import ReadTheDocsLoader
 from langchain_community.document_loaders import PyPDFLoader
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.pinecone import Pinecone
